e633ecace76d1ecae9225c9e7244f4d74724995d
Erik Høyrup Jørgensen <ErikHoyJor@gmail.com>
2014-11-25 17:32:11 +0100

    Init

---
+++ ChunkAnalyser/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChunkAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region args
+            if (args.Length == 0)
+            {
+                Console.Write("Chunk Creator - ");
+                Console.Write("Erik Høyrup Jørgensen - ");
+                Console.WriteLine("Stejska.com - 2014");
+                Console.WriteLine();
+                Console.WriteLine("Format: [OPTIONAL OPTIONS]");
+                Console.WriteLine();
+                Console.WriteLine("Options:");
+                Console.WriteLine("-c\tChar set file path");
+                Console.WriteLine("-s\tChunkSize");
+                Console.WriteLine("-l\tLength of password");
+                Console.WriteLine("-d\tdirectory");
+                Console.ReadKey();
+            }
+            #endregion
+        }
+    }
+}
